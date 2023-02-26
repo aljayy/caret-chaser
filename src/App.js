b19580e1d44d0a5e1e19d1213ed665b0836cd5e8
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-02-25 16:06:25 -0800

    feat(App): add Passage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
 import classes from "./App.module.scss";
+import Passage from "./components/Passage/Passage";
 
 function App() {
   return (
     <>
       <Header />
+      <Passage />
     </>
   );
 }
