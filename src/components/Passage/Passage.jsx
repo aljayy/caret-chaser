a3a93ea22a561ffacffed1e321ad6985dbdc59fd
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-02-28 12:55:52 -0800

    feat: add Caret component functionality
    This implements the functionality that with every letter
    typed, the caret component's position is changed alongside
    with it to follow the user typing. This is the implementation
    for traversing the x axis.

--- src/components/Passage/Passage.jsx
+++ src/components/Passage/Passage.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from "react";
+import Caret from "../Caret/Caret";
 import classes from "./Passage.module.scss";
 
 function Passage() {
   const [wordIndex, setWordIndex] = useState(0);
   const [letterIndex, setLetterIndex] = useState(0);
   const [passageArray, setPassageArray] = useState([]);
+  const [caretX, setCaretX] = useState(0.4);
+  // Caret component implementation
+  // // This component is going to be a div with no content. Just some width, height, and coloring added to give the appearance of a caret.
+  // // // Currently positioned absolute with parent "passage-wrapper" positioned relative. With a starting top of 0.8rem and left 0.4rem
+  // // // After some light testing, the caret's left position is going to have to move 1.2rem per letter typed. 1rem when space bar is clicked.
 
   useEffect(() => {
     return async () => {
@@ -44,6 +50,7 @@ function Passage() {
         });
       }
       setLetterIndex((prev) => prev + 1);
+      setCaretX((prev) => prev + 1.2);
     } else if (e.key === " ") {
       if (letterIndex === 0) return;
 
@@ -61,6 +68,7 @@ function Passage() {
 
       setWordIndex((count) => count + 1);
       setLetterIndex(0);
+      setCaretX((prev) => prev + 1);
     } else if (e.key === "Backspace" && letterIndex > 0) {
       setLetterIndex((prev) => prev - 1);
       setPassageArray((prev) => {
@@ -69,11 +77,13 @@ function Passage() {
         updated[wordIndex][letterIndex - 1].incorrect = null;
         return updated;
       });
+      setCaretX((prev) => prev - 1.2);
     }
   }
 
   return (
     <div className={classes["passage-wrapper"]}>
+      <Caret caretX={caretX} />
       <input className={classes["test-input"]} onKeyDown={checkLetter} />
       {passageArray.map((word) => {
         return (
