c7325e714fa0c80815d3f8ad9fe103546a82c6d1
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-02 16:20:15 -0800

    feat: add theme changing functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { useContext } from "react";
 import Header from "./components/Header/Header";
-import classes from "./App.module.scss";
 import Passage from "./components/Passage/Passage";
+import ThemeCtx from "./context/themectx";
+import classes from "./App.module.scss";
 
 function App() {
+  const themeCtx = useContext(ThemeCtx);
+
+  const theme = themeCtx.theme === "light" ? classes.light : classes.dark;
+
   return (
-    <>
+    <div className={theme}>
       <Header />
       <Passage />
-    </>
+    </div>
   );
 }
 
