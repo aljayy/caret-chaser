da37791cc97da201811f2a5421a91e16b179e958
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-04 11:10:43 -0800

    feat: consume theme context state
    This is done in order to know which logo to render
    depending on the theme

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -1,7 +1,14 @@
-import logo from "../../../assets/cclogo.svg";
+import { useContext } from "react";
+import ThemeCtx from "../../../context/themectx";
+import lightLogo from "../../../assets/cclightlogo.svg";
+import darkLogo from "../../../assets/ccdarklogo.svg";
 import classes from "./Logo.module.scss";
 
 function Logo() {
+  const { theme } = useContext(ThemeCtx);
+
+  const logo = theme === "light" ? lightLogo : darkLogo;
+
   return (
     <div className={classes["logo-wrapper"]}>
       <img src={logo} alt="Logo" />
