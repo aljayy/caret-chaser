3a2d0fcefa59818787050e758097ab322489d4d1
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-04 11:21:34 -0800

    feat: consume theme context state for theme

--- src/components/TestSettings/MobileTestSettings/MobileTestSettings.jsx
+++ src/components/TestSettings/MobileTestSettings/MobileTestSettings.jsx
@@ -1,11 +1,19 @@
-import gear from "../../../assets/lightsettingsicon.svg";
+import { useContext } from "react";
+import ThemeCtx from "../../../context/themectx";
+import lightgear from "../../../assets/lightsettingsicon.svg";
+import darkgear from "../../../assets/darksettingsicon.svg";
 import classes from "./MobileTestSettings.module.scss";
 
 function MobileTestSettings() {
+  const themeCtx = useContext(ThemeCtx);
+
+  const theme = themeCtx.theme === "light" ? classes.light : classes.dark;
+  const gear = themeCtx.theme === "light" ? lightgear : darkgear;
+
   return (
-    <button className={classes["mobile-settings"]}>
+    <button className={`${classes["mobile-settings"]} ${theme}`}>
       <div className={classes.icon}>
-        <img src={gear} />
+        <img src={gear} alt={"settings icon"} />
       </div>
       <h2>Test Settings</h2>
     </button>
