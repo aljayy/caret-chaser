66f126bfff0f2a4a61486de6f69f576acbcce806
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-05 10:10:24 -0800

    style: add classes for theme state

--- src/components/Caret/Caret.jsx
+++ src/components/Caret/Caret.jsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
+import ThemeCtx from "../../context/themectx";
 import classes from "./Caret.module.scss";
 
 function Caret({ caretLeft, caretTop }) {
+  const themeCtx = useContext(ThemeCtx);
+
+  const theme = themeCtx.theme === "light" ? classes.light : classes.dark;
+
   return (
     <div
-      className={classes.caret}
+      className={`${classes.caret} ${theme}`}
       style={{ left: `${caretLeft}rem`, top: `${caretTop}rem` }}
     />
   );
