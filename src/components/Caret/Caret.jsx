1ec04f984d037a864a62789fe845c3c8806ad26e
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-07 13:50:24 -0800

    Revert "refactor: move state updating functions to testctx.js context file"
    This reverts commit 333118838eafe115ce63284abb853255896dcf26.

--- src/components/Caret/Caret.jsx
+++ src/components/Caret/Caret.jsx
@@ -1,11 +1,9 @@
 import { useContext } from "react";
 import ThemeCtx from "../../context/themectx";
-import TestCtx from "../../context/testctx";
 import classes from "./Caret.module.scss";
 
-function Caret() {
+function Caret({ caretLeft, caretTop }) {
   const themeCtx = useContext(ThemeCtx);
-  const { caretLeft, caretTop } = useContext(TestCtx);
 
   const theme = themeCtx.theme === "light" ? classes.light : classes.dark;
 
