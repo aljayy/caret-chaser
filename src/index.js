8720c32a12cbc6b51b0ccd496c30d9438ac0c3a5
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-07 13:54:35 -0800

    feat: provide test context

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { TestCtxProvider } from "./context/testctx";
 import { ThemeCtxProvider } from "./context/themectx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeCtxProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <TestCtxProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </TestCtxProvider>
   </ThemeCtxProvider>
 );
