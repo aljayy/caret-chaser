333118838eafe115ce63284abb853255896dcf26
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-07 13:48:54 -0800

    refactor: move state updating functions to testctx.js context file

--- src/components/Caret/Caret.jsx
+++ src/components/Caret/Caret.jsx
@@ -1,9 +1,11 @@
 import { useContext } from "react";
 import ThemeCtx from "../../context/themectx";
+import TestCtx from "../../context/testctx";
 import classes from "./Caret.module.scss";
 
-function Caret({ caretLeft, caretTop }) {
+function Caret() {
   const themeCtx = useContext(ThemeCtx);
+  const { caretLeft, caretTop } = useContext(TestCtx);
 
   const theme = themeCtx.theme === "light" ? classes.light : classes.dark;
 
