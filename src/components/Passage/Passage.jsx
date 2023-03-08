167333a45f6ae65afe91f3238d6d2c496cf7455c
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-08 12:37:14 -0800

    feat: add timer component

--- src/components/Passage/Passage.jsx
+++ src/components/Passage/Passage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useContext } from "react";
 import TestCtx from "../../context/testctx";
 import ThemeCtx from "../../context/themectx";
 import Caret from "../Caret/Caret";
+import Timer from "../UI/Timer";
 import classes from "./Passage.module.scss";
 
 function Passage() {
@@ -16,38 +17,41 @@ function Passage() {
   }, [retrievePassage]);
 
   return (
-    <div className={`${classes["test-wrapper"]} ${theme}`}>
-      <Caret />
-      <input
-        className={classes["test-input"]}
-        onKeyDown={checkLetter}
-        spellCheck={false}
-      />
-      <div
-        ref={passageRef}
-        className={classes["passage-wrapper"]}
-        style={{ top: `${passageTop}rem` }}
-      >
-        {passageArray.map((word, index) => {
-          return (
-            <div className={classes.word} key={`${word + index}`}>
-              {word.map((current, index) => {
-                return (
-                  <span
-                    className={`${current.correct ? classes.correct : ""} ${
-                      current.incorrect ? classes.incorrect : ""
-                    }`}
-                    key={`${current + index}`}
-                  >
-                    {current.letter}
-                  </span>
-                );
-              })}
-            </div>
-          );
-        })}
+    <>
+      <Timer />
+      <div className={`${classes["test-wrapper"]} ${theme}`}>
+        <Caret />
+        <input
+          className={classes["test-input"]}
+          onKeyDown={checkLetter}
+          spellCheck={false}
+        />
+        <div
+          ref={passageRef}
+          className={classes["passage-wrapper"]}
+          style={{ top: `${passageTop}rem` }}
+        >
+          {passageArray.map((word, index) => {
+            return (
+              <div className={classes.word} key={`${word + index}`}>
+                {word.map((current, index) => {
+                  return (
+                    <span
+                      className={`${current.correct ? classes.correct : ""} ${
+                        current.incorrect ? classes.incorrect : ""
+                      }`}
+                      key={`${current + index}`}
+                    >
+                      {current.letter}
+                    </span>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
