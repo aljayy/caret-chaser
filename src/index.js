7acb3388861f73563b21c7fc8bff6202fc29e85b
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-09 09:49:25 -0800

    feat: add HashRouter to load paths

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { TestCtxProvider } from "./context/testctx";
 import { ThemeCtxProvider } from "./context/themectx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ThemeCtxProvider>
-    <TestCtxProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </TestCtxProvider>
-  </ThemeCtxProvider>
+  <React.StrictMode>
+    <HashRouter>
+      <ThemeCtxProvider>
+        <TestCtxProvider>
+          <App />
+        </TestCtxProvider>
+      </ThemeCtxProvider>
+    </HashRouter>
+  </React.StrictMode>
 );
