3ddfe327b89babceb7465d080bdc272d72512720
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-14 11:29:32 -0700

    feat: add functionality for the mobile menu test settings

--- src/components/TestSettings/MobileTestSettings/MobileTestSettings.jsx
+++ src/components/TestSettings/MobileTestSettings/MobileTestSettings.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import ThemeCtx from "../../../context/themectx";
 import lightgear from "../../../assets/lightsettingsicon.svg";
 import darkgear from "../../../assets/darksettingsicon.svg";
@@ -6,17 +6,46 @@ import classes from "./MobileTestSettings.module.scss";
 
 function MobileTestSettings() {
   const themeCtx = useContext(ThemeCtx);
+  const [showMenu, setShowMenu] = useState(true);
 
   const theme = themeCtx.theme === "light" ? classes.light : classes.dark;
   const gear = themeCtx.theme === "light" ? lightgear : darkgear;
 
+  function toggleMenu(e) {
+    e.bubbles = false;
+    console.log(e);
+    setShowMenu((prev) => !prev);
+  }
+
   return (
-    <button className={`${classes["mobile-settings"]} ${theme}`}>
-      <div className={classes.icon}>
-        <img src={gear} alt={"settings icon"} />
-      </div>
-      <h2>Test Settings</h2>
-    </button>
+    <>
+      {!showMenu && (
+        <button
+          onClick={toggleMenu}
+          className={`${classes["mobile-settings"]} ${theme}`}
+        >
+          <div className={classes.icon}>
+            <img src={gear} alt={"settings icon"} />
+          </div>
+          <h2>Test Settings</h2>
+        </button>
+      )}
+      {showMenu && (
+        <div className={classes.overlay} onClick={toggleMenu}>
+          <menu className={classes["mobile-menu"]}>
+            <ul>
+              <li>test</li>
+              <li>quotes</li>
+            </ul>
+            <ul>
+              <li>15</li>
+              <li>30</li>
+              <li>60</li>
+            </ul>
+          </menu>
+        </div>
+      )}
+    </>
   );
 }
 
