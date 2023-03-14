5bdf3ad62b8ceb09b61a8fb41804d91339150f3d
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-14 15:35:40 -0700

    feat: add state functionality for switching test type

--- src/components/TestSettings/MobileTestSettings/MobileTestSettings.jsx
+++ src/components/TestSettings/MobileTestSettings/MobileTestSettings.jsx
@@ -6,13 +6,47 @@ import classes from "./MobileTestSettings.module.scss";
 
 function MobileTestSettings() {
   const themeCtx = useContext(ThemeCtx);
-  const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(true);
+  const [settings, setSettings] = useState([
+    [
+      { test: "test", active: true },
+      { test: "quotes", active: false },
+    ],
+    [
+      { settings: 15, active: true },
+      { settings: 30, active: false },
+      { settings: 60, active: false },
+    ],
+  ]);
 
   const theme = themeCtx.theme === "light" ? classes.light : classes.dark;
   const gear = themeCtx.theme === "light" ? lightgear : darkgear;
+  const menuTheme =
+    themeCtx.theme === "light" ? classes["menu-light"] : classes["menu-dark"];
+  const menuActiveTheme =
+    themeCtx.theme === "light"
+      ? classes["category-active-light"]
+      : classes["category-active-dark"];
+  const menuInactiveTheme =
+    themeCtx.theme === "dark"
+      ? classes["category-inactive-dark"]
+      : classes["category-inactive-light"];
 
   function toggleMenu(e) {
     setShowMenu((prev) => !prev);
+  }
+
+  function selectSettings(setState, array, i) {
+    setState((prev) => {
+      let updated = [...prev];
+      console.log(updated[array]);
+      updated[array].map((category, index) => {
+        if (index === i) return (category.active = true);
+        else return (category.active = false);
+      });
+
+      return updated;
+    });
   }
 
   return (
@@ -31,17 +65,36 @@ function MobileTestSettings() {
       {showMenu && (
         <div className={classes.overlay} onClick={toggleMenu}>
           <menu
-            className={classes["mobile-menu"]}
+            className={`${classes["mobile-menu"]} ${menuTheme}`}
             onClick={(e) => e.stopPropagation()}
           >
             <ul>
-              <li>test</li>
-              <li>quotes</li>
+              {settings[0].map((category, index) => {
+                return (
+                  <li
+                    className={`${
+                      category.active ? menuActiveTheme : menuInactiveTheme
+                    }`}
+                    onClick={() => selectSettings(setSettings, 0, index)}
+                  >
+                    {category.test}
+                  </li>
+                );
+              })}
             </ul>
             <ul>
-              <li>15</li>
-              <li>30</li>
-              <li>60</li>
+              {settings[1].map((category, index) => {
+                return (
+                  <li
+                    className={`${
+                      category.active ? menuActiveTheme : menuInactiveTheme
+                    }`}
+                    onClick={() => selectSettings(setSettings, 1, index)}
+                  >
+                    {category.settings}
+                  </li>
+                );
+              })}
             </ul>
           </menu>
         </div>
